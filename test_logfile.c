c93990885136f1bb988c7d99070e2fc2251e8bf9
Ernold Puvlist <epuvlist@gmail.com>
2024-07-26 11:01:29 +0100

    Minor changes

--- test_logfile.c
+++ test_logfile.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
 #include "logfile.h"
 
+/* This program tests the ability of the logfile.c program
+   to truncate the log file when it reaches the length limit.
+   logfile.c is best compiled with the -DDEBUG option to see the 
+   full effect of the test. */
+
 int main() {
 
     // test
-    char my_log[] = "nedlog.txt";
+    char my_log[] = "nedlog.log";
     char numberstring[4];
 
     /* Write 8 lines to the log file, but limit it to 5 lines,
